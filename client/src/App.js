d131a3cd73c04cd64c92935b8f51598519d68ebb
Tony Phondeth <phondeth@gmail.com>
2021-11-07 16:18:09 -0500

    feature/forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+
 
 import Header from './components/Header';
 import Footer from './components/Footer';
